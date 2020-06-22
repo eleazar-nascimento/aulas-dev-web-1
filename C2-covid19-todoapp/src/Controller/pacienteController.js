1a0914ed8bf71972349d6838aa6356c4c87afc61
zarathos-sky <zarathos.sky@gmail.com>
2020-06-22 19:18:48 -0300

    Estruturação Padrão MVC

--- C2-covid19-todoapp/src/Controller/pacienteController.js
+++ C2-covid19-todoapp/src/Controller/pacienteController.js
@@ -1,16 +1,3 @@
-var selectedRow = null
-
-function onFormSubmit() {
-    if (validate()) {
-        var formData = readFormData();
-        if (selectedRow == null)
-            insertNewRecord(formData);
-        else
-            updateRecord(formData);
-        resetForm();
-    }
-}
-
 function readFormData() {
     var formData = {};
     formData["nome"] = document.getElementById("nome").value;
